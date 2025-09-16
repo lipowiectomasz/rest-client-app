11371773a63a0f0c951ee9224ce910b758aae26f
Kathering Rivera <katheriverar@gmail.com>
2025-09-16 13:27:32 -0500

    feat: add name and email to mainPage if authenticated user

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,18 +5,24 @@ import Link from 'next/link';
 export default async function Home() {
   const t = await getTranslations();
 
-  const authenticated = await auth();
+  const session = await auth();
 
-  if (authenticated) {
+  if (session) {
+    const { name, email } = session.user || {};
     return (
       <>
         <div className="max-w-screen-lg mx-auto">
           <section className="w-full px-4 py-16 min-h-100 sm:mt-[15vmax] md:mt-[10vmax] dark:bg-slate-800 bg-slate-50 rounded-t-lg inset-shadow-sm inset-shadow-slate-300 dark:inset-shadow-gray-600">
-            <h1 className="text-center text-3xl">{t('mainPage.title')}</h1>
+            <h1 className="text-center text-3xl">{t('mainPage.title.in')} </h1>
+            {/* Mostrar nombre y email */}
+            <div className="text-center mt-4">
+              <div>{name}</div>
+              <div>{email}</div>
+            </div>
           </section>
-          <section className="flex gap-6 items-center justify-evenly dark:bg-slate-800 bg-slate-100 rounded-b-lg shadow-md shadow-slate-600">
+          <section className="flex gap-6 items-center justify-evenly text-center dark:bg-slate-800 bg-slate-100 rounded-b-lg shadow-md shadow-slate-600">
             <Link
-              href="/rest"
+              href="/rest-client"
               className="hover:bg-slate-50 hover:rounded-bl-lg w-full py-6 dark:hover:bg-slate-900"
             >
               <i>REST</i> Client
@@ -38,11 +44,12 @@ export default async function Home() {
       </>
     );
   }
+
   return (
     <>
       <div className="max-w-screen-lg mx-auto">
         <section className="flex flex-col gap-3 items-center justify-center w-full px-4 py-16 min-h-100 sm:mt-[15vmax] md:mt-[10vmax] dark:bg-slate-800 bg-slate-50 rounded-t-lg inset-shadow-sm inset-shadow-slate-300 dark:inset-shadow-gray-600">
-          <h1 className=" text-3xl">{t('mainPage.title')}</h1>
+          <h1 className=" text-3xl">{t('mainPage.title.out')}</h1>
           <h2 className="">{t('mainPage.subtitle')}</h2>
         </section>
         <section className="flex gap-6 items-center justify-evenly dark:bg-slate-800 bg-slate-100 rounded-b-lg shadow-md shadow-slate-600">
