c52a112123431db67a45f9e12773ecfe98cbc65f
Denis <me@email>
2025-09-19 12:21:45 +0300

    refactor: added additional styles for buttons and fields

--- src/components/rest-client/UrlInput.tsx
+++ src/components/rest-client/UrlInput.tsx
@@ -21,7 +21,7 @@ export function UrlInput({ value, onChange }: { value: string; onChange: (url: s
       value={url}
       onChange={(e) => handleChange(e.target.value)}
       placeholder="https://api.example.com"
-      className="w-full px-2 my-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 bg-slate-50 rounded-md inset-shadow-sm inset-shadow-slate-300 dark:inset-shadow-gray-600"
+      className="w-full px-2 my-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 bg-slate-50 rounded-md inset-shadow-sm inset-shadow-slate-300 dark:inset-shadow-gray-600 text-black"
     />
   );
 }
